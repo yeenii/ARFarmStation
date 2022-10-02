603ee08a055228a6c54314e77748d4e3cdcda192
yeenii <jye0042@naver.com>
2022-10-03 04:12:35 +0900

    AR Settings commit
    AR 세팅 커밋

---
+++ FarmStation_Vuforia/Assets/ButtonController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eeb8857eb6bda83fb730186c6b4931d6ad687da2f793758e6cd53c4697566a76
+size 434
---
+++ FarmStation_Vuforia/Assets/Editor/Migration/AddVuforiaEnginePackage.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:496ea5a3be2772a0e8e4db3f5592f7b500a22e7c06ae0ac0862667c80a72df3a
+size 16563
